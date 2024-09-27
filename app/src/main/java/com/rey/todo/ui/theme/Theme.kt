6ae4a68843df61ef95d168883a1daed79fa943ce
Shreyas Patil <shreyassp002@gmail.com>
2024-09-27 14:54:43 +0530

    Added Custom colors

--- app/src/main/java/com/rey/todo/ui/theme/Theme.kt
+++ app/src/main/java/com/rey/todo/ui/theme/Theme.kt
@@ -12,15 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimary,
+    secondary = DarkSecondary,
+    tertiary = DarkTertiary,
+    background = DarkBackground,
+    surface = DarkSurface,
+    onSurface = DarkOnSurface,
+    surfaceVariant = DarkSurfaceVariant
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightPrimary,
+    secondary = LightSecondary,
+    tertiary = LightTertiary,
+    background = LightBackground,
+    surface = LightSurface,
+    onSurface = LightOnSurface,
+    surfaceVariant = LightSurfaceVariant
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
